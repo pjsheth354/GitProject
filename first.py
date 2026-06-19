4bb1202abfbc4c9a28c479eb0e70ebdfeed40619
pjsheth354 <priyamsheth354@gmail.com>
2026-06-19 19:28:58 -0400

    2nd and 3rd day

--- first.py
+++ first.py
@@ -1,1 +1,35 @@
-print("Hello World!")
+#This is dedicated for text
+print("Hello World!\tMy Name is Priyam")
+print("")
+
+"""
+    This is a 
+    Multiline Comment 
+    Example
+"""
+
+
+#This is dedicated for numbers
+a = 10
+b = 15
+c = a + b
+print('My age is',c,'and have attended Lakehead University')
+
+#Casting method
+x = 3 
+y = float (x)
+z = str (x)
+print(x,y,z)
+print(type(x),type(y),type(z))
+
+#If else statement
+if (a == 10):
+    print(True)
+else:
+    print(False)
+
+#String repitation
+x = "Priyam"
+y = "Sheth"
+print (x*2)
+print (x + " " + y)
