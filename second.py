e79083697d26829a23f54b2f13c5456466326544
pjsheth354 <priyamsheth354@gmail.com>
2026-06-19 21:29:06 -0400

    Day 2 and Day 3

---
+++ second.py
@@ -0,0 +1,7 @@
+#List Methods
+fruits = ['Apple','Mango','Banana']
+veggies = ['Cucumber','Carrot','Cabbage']
+
+mergedList = fruits + veggies
+
+print(mergedList)
---
+++ third.py
@@ -0,0 +1,13 @@
+#String Operations
+
+str1 = "Hello"
+str2 = "My Name is Priyam"
+str3 = "Have a Good Day!!"
+
+print(str1 + " " + str2 + " "+ str3)
+print(str1.capitalize())
+print(str1.upper())
+print(str1.lower())
+print(str1.swapcase())
+print(str1.title())
+print(str1.center(24))
